3237ec91860430ec0e3d4d9cbc41638d4b4233c0
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-09 21:42:32 -0300

    testes js

---
+++ 5_number/js/scripts.js
@@ -0,0 +1,15 @@
+var numero = 10;
+
+console.log(numero);
+console.log(typeof numero);
+
+
+var float = 5.4;
+
+console.log(float);
+console.log(typeof float);
+
+var textoComNumero = '200';
+
+console.log(textoComNumero);
+console.log(typeof textoComNumero);
