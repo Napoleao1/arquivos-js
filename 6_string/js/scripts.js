4b675f8a43f895f7724357d26a542286bbec5dc5
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-10 17:44:21 -0300

    estudos js

---
+++ 6_string/js/scripts.js
@@ -0,0 +1,23 @@
+var nome = "napoleão";
+var sobrenome = "luz";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "abcd");
+
+console.log(nomeCompleto);
+
+console.log(typeof "123412");
+
+var frase = 'Esta é uma frase complexa';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write("eai");
+document.write("ele disse 'ola' ")
