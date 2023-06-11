1a7aa210361cb79d6bd721aa872ae71e94570dc2
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-11 13:23:28 -0300

    estudos de js

---
+++ 9_objetos/js.scripts.js
@@ -0,0 +1,23 @@
+var obj = {
+    nome: "Napoleão",
+    idade: 22,
+    profissão: "Programador",
+    estaTrabalhando: true,
+};
+console.log(obj)
+console.log(typeof obj)
+
+console.log(obj.nome);
+
+console.log("o meu nome é " + obj.nome)
+
+obj.nome = "Ernani";
+console.log(obj.nome);
+console.log(obj);
+
+obj.graduacao = true;
+console.log(obj);
+
+obj.idade = "23";
+console.log(obj.idade);
+console.log(obj);
