5801b7eaeec93c8154d9dbf0ca6a579cb391e657
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-12 20:37:47 -0300

    teste 2 if

--- 11_if/js/scripts.js
+++ 11_if/js/scripts.js
@@ -1,7 +1,13 @@
+var idade = 16
+
 console.log("Antes do if");
 
-if(false){
-    console.log("testando o if");
+if(idade > 18){
+    console.log("pode servir o exército");
+}
+
+if(idade>15){
+    console.log("Precisa esperar 3 anos ainda")
 }
 
 console.log("Depois do if");
