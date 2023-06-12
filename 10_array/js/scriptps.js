584b2dd8f3644307fba2964b5d3948d5175e49ef
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-12 20:30:50 -0300

    estudos do js

---
+++ 10_array/js/scriptps.js
@@ -0,0 +1,11 @@
+var arr = [ 2, "Napoleão", true, {teste: 3, teste: 5}];
+console.log(arr);
+
+var arr2 =[1,2,3,4,5];
+console.log(arr2);
+
+console.log(arr[3]);
+
+
+arr[4] = 'Lol';
+console.log(arr);
---
+++ 11_if/js/scripts.js
@@ -0,0 +1,7 @@
+console.log("Antes do if");
+
+if(false){
+    console.log("testando o if");
+}
+
+console.log("Depois do if");
