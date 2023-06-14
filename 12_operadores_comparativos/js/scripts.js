4801ca8740871b4709180832f664bc9ed5feb0dd
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-14 12:07:52 -0300

    estudos js operador

---
+++ 12_operadores_comparativos/js/scripts.js
@@ -0,0 +1,34 @@
+var festa = 15;
+var beber = 1
+
+if(festa >= 18){
+console.log("o usuário já pode ir em festas");
+}
+
+if(festa <= 17){
+    console.log("o usuário não pode ir em festas");
+}
+
+if(beber){
+    console.log("o usuário ja pode beber a vontade");
+}
+
+/* operador de igualdade */
+
+var nome = "lucas";
+
+if(nome == "Ernani"){
+    console.log("O seu nome é ernani");
+}
+
+if(nome != "Ernani"){
+    console.log("Eii o seu nome não é ernani.. Qual será que é seu nome?");
+}
+
+if(20 > 5){
+    console.log("Sim");
+}
+
+if(200 < 10){
+    console.log("Não");
+}
