5f9443311b8e755c75fe234422c6556e587a769f
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-15 19:19:38 -0300

    testando operadores lógicos

--- 16_or/js/scripts.js
+++ 16_or/js/scripts.js
@@ -1,6 +1,16 @@
-var idade = 20;
-var nome = 'Ernani';
+var idade = 15;
+var nome = "pedro";
 
-if(nome == 'ernani' || idade > 15 );{
-    console.log("Vai poder entrar nas aulas de boxe")
+
+
+if(nome == "joão" || idade > 14) {
+    console.log("Vai poder entrar nas aulas de boxe");
+}   else{
+    console.log("Não pode entrar")
+}
+
+if(nome == "pedro" && (15 > 20 || 9 == 10)){
+    console.log("Testando");
+} else{
+    console.log("Não entrou")
 }
