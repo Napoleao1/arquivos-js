30ba2c6913cf4361f926ca456d6fe3c353296555
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-16 20:20:38 -0300

    while

--- 18_while/js/scripts.js
+++ 18_while/js/scripts.js
@@ -1,7 +1,22 @@
 var y = 0;
 
-while(y < 4){
+while(y <+ 4){
     console.log("Testando a repetição " + y);
 
     y++;
+}
+
+var arr = ['banana', 'maça', 'melão', 'abacate'];
+var x = 0;
+while( x <= 3){
+    console.log(arr[x]);
+    x++;
+}
+
+var nome = "Ernani"; 
+var i = 0;
+
+while( i < 6){
+    console.log(nome[i]);
+    i += 1;
 }
