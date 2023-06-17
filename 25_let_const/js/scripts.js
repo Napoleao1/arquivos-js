b97397e0e132e36515a36165ac0b8cce9c62869d
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-17 20:55:17 -0300

    const e let tests

---
+++ 25_let_const/js/scripts.js
@@ -0,0 +1,19 @@
+let nome = 'ernani'; // var nome = 'ernani;
+
+let x = 5; // var x = 5;
+
+const y = 10;
+
+x = 12;
+
+console.log('let ' + x);
+console.log('const ' + y);
+if(true) {
+    let x = 20;
+
+    console.log('let ' + x)
+
+    const y = 15;
+    console.log('const ' + y);
+}
+console.log('let ' + x)
