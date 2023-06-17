71df68b1eef4d5a2b8522092351606182d711443
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-17 18:32:37 -0300

    study with returne function

---
+++ 22_funções/js/scripts.js
@@ -0,0 +1,39 @@
+function primeiraFuncao() {
+    console.log("Hello word das funções");
+}
+primeiraFuncao();
+// ---------------------- // 
+
+function dizerNome(nome){
+    console.log("o nome é: " + nome);
+}
+
+dizerNome("Ernani");
+dizerNome("Rafael");
+dizerNome("Luiz");
+
+var nomeDoBancoDeDados = "João";
+dizerNome(nomeDoBancoDeDados);
+
+
+
+function nomeJogo(jogo){
+    console.log("O nome do jogo é: " + jogo);
+}
+
+var nomeDoJogo = "lol";
+nomeJogo(nomeDoJogo);
+
+
+// função de soma //
+
+function soma(a, b) {
+    var soma = a + b;
+    return soma;
+}
+
+var somaUm = soma(2, 5);
+console.log(somaUm);
+
+var somaDois = soma ( 10, 24);
+console.log(somaDois);
