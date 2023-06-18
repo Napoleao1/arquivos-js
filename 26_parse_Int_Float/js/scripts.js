84fbf1eeb9acbf0433989aa277b0d80e835e1d54
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-17 21:18:15 -0300

    isNaN testes

--- 26_parse_Int_Float/js/scripts.js
+++ 26_parse_Int_Float/js/scripts.js
@@ -10,3 +10,14 @@ console.log(parseInt(13.320));
 //toFixed
 
 console.log(22.34353.toFixed(4));
+
+
+//isNaN
+
+console.log(isNaN('true'));
+
+console.log(isNaN(20));
+
+console.log(isNaN('30'));
+
+// MAX_VALUE E MIN_VALUE
