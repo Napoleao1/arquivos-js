69b8cb437c11dd871fe7106435261dcc86280400
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-19 22:14:43 -0300

    toUpperCase toLowerCase e trim

---
+++ 28_metodos_string_2/js/scripts.js
@@ -0,0 +1,15 @@
+var frase = "Está é a frase que vou modificar";
+
+var fraseCaixaAlta = frase.toUpperCase();
+
+console.log(fraseCaixaAlta);
+
+console.log(fraseCaixaAlta.toLowerCase());
+
+// trim 
+
+var nome = "         Ernani         ";
+
+var nomeTrim = nome.trim();
+console.log(nome);
+console.log(nomeTrim);
