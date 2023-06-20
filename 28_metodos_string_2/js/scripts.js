521a9e7f7e08495a94704e3012517b40a5d99729
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-19 22:22:23 -0300

    split e lastIndexOf

--- 28_metodos_string_2/js/scripts.js
+++ 28_metodos_string_2/js/scripts.js
@@ -13,3 +13,18 @@ var nome = "         Ernani         ";
 var nomeTrim = nome.trim();
 console.log(nome);
 console.log(nomeTrim);
+
+// split
+
+console.log(frase.split(" "));
+
+var idiomas = "Inglês, Espanhol, Português, Japonês, Italiano";
+
+console.log(idiomas.split(", "));
+
+// last index of
+
+var fraseRepetida = "eu tenho um computador rápido e um outro computador mais lento mas foi o meu primeiro computador";
+
+console.log(fraseRepetida.indexOf("computador"));
+console.log(fraseRepetida.lastIndexOf("computador"))
