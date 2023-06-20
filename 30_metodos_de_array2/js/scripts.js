456d0180dac45f4952a6f45010b12c6554785937
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-20 20:10:30 -0300

    indexOf

--- 30_metodos_de_array2/js/scripts.js
+++ 30_metodos_de_array2/js/scripts.js
@@ -1,9 +1,14 @@
 // splice
 
-var arr = [1,2,3,4,5,6];
-arr.splice(3, 0,59595959);
+var arr = [1,2,3,4,5];
+arr.splice(2, 0, 59595959);
 
 console.log(arr);
 
 arr.splice(4, 1);
 console.log(arr);
+
+
+//indexOf
+
+console.log(arr.indexOf(3));
