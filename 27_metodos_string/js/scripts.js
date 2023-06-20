ff5bba9f989ea01f8b528375860b3aa4879ee922
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-19 22:03:59 -0300

    métodos de string, leght, indexof, slice e replace

---
+++ 27_metodos_string/js/scripts.js
@@ -0,0 +1,16 @@
+var nome = 'Ernani';
+console.log(nome.length);
+
+console.log(nome[0]);
+
+var frase = 'estou estudando sobre javascript';
+console.log(frase.indexOf('sobre'));
+
+
+var tempo = "choveu ontem a noite";
+var choveu = tempo.slice(0, 6);
+console.log(choveu);
+
+
+var novoTempo = tempo.replace("choveu", "fez frio");
+console.log(novoTempo);
