e454cdf5c8d31e6dfd9185d9339ca2fe6e07cbcc
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-23 19:43:54 -0300

    innerHtml e textContent

---
+++ 35_alterarConteudo/js/scripts.js
@@ -0,0 +1,15 @@
+var title = document.querySelector("#title");
+
+
+// innerHTML;
+title.innerHTML = "Alterei o título";
+
+// textContent 
+
+var paragrafo = document.querySelector("#paragrafo");
+paragrafo.innerHTML = "Mudando o meu parágrafo"
+
+
+var subtitle = document.querySelector(".subtitle");
+
+subtitle.textContent = " ALTEREI O SEGUNDO"
