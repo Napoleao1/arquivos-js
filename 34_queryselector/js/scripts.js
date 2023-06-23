0f882cf45205883c3ee289832fd3138ba2861867
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-22 23:25:39 -0300

    query

---
+++ 34_queryselector/js/scripts.js
@@ -0,0 +1,21 @@
+var itensClasse = document.getElementsByClassName('item')
+console.log(itensClasse);
+
+var itensQuery = document.querySelectorAll('#lista2 li');
+
+console.log(itensQuery);
+
+
+var itensQuery2 = document.querySelectorAll('#lista .item');
+
+console.log(itensQuery2);
+
+
+// querySelector para pegar somente um elemento
+
+var  itensQuery2 = document.querySelector('#lista .item')
+
+console.log(itensQuery2);
+
+var span = document.querySelector(' #paragrafo span');
+console.log(span);
