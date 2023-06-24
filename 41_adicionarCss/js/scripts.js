4e6c8aff04a18a3a0cbc1eb830fa4a1d99cc8fb0
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-24 19:16:35 -0300

    add styles with js

---
+++ 41_adicionarCss/js/scripts.js
@@ -0,0 +1,11 @@
+var title = document.querySelector("#title");
+
+
+// add estilo
+title.style.color = "blue";
+
+title.style.backgroundColor = "Black";
+
+var p = document.querySelector("#paragrafo");
+
+p.style.cssText = "Color: blue; background-color: pink; font-size: 100px";
