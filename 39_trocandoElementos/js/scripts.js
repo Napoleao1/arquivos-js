ebe2bc0f472ea0fcd6f1523cd00336e267fcf990
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-23 23:03:17 -0300

    elemento pai e trocando elementos.

--- 39_trocandoElementos/js/scripts.js
+++ 39_trocandoElementos/js/scripts.js
@@ -10,3 +10,31 @@ elemento.appendChild(texto);
 console.log(elemento);
 
 // selecionando o elemento que quero trocar
+
+var title = document.querySelector("#title");
+console.log(title);
+
+// selecionar o pai do elemento
+
+// var body = document.querySelector("body");
+ var pai = title.parentNode; // para puxar o  elemento pai
+
+// trocar os elementos
+
+pai.replaceChild(elemento, title); // troquei o h1 pelo h3
+
+
+var paragrafo = document.createElement("p");
+
+paragrafo.classList = "classe-paragrafo";
+
+var texto2 = document.createTextNode("testando agora com paragráfos");
+
+paragrafo.appendChild(texto2);
+
+console.log(paragrafo);
+
+var pai2 = elemento.parentNode;
+
+pai2.replaceChild(paragrafo, elemento);
+
