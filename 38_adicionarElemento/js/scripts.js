669f0c77e37a79ec4f1b0cbfd974b73dea466dc7
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-23 21:46:04 -0300

    create elements and inserting

--- 38_adicionarElemento/js/scripts.js
+++ 38_adicionarElemento/js/scripts.js
@@ -5,7 +5,7 @@ var elemento = document.createElement("div");
 
 elemento.classList = "div-criada"
 
-console.log(elemento);
+
 
 
 // inserindo no container elemento filho
@@ -13,5 +13,21 @@ console.log(elemento);
 var container = document.querySelector("#container");
 
 container.appendChild(elemento);
+
+//inserting before  - antes
+
+var elemento2 = document.createElement("div");
+
+elemento2.classList = "before";
+
+var elemento3 = document.querySelector("#container .div-criada");
+console.log(elemento3);
+
+container.insertBefore(elemento2, elemento3);
+
+var paragrafo = document.createElement("p");
+console.log(paragrafo);
+
+container.insertBefore(paragrafo, elemento2);
 
 
