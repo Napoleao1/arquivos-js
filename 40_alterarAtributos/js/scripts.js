a0c0e16653f9b2068e2b2e82418c6dc2b8b74079
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-24 17:02:01 -0300

    add class atributes for elements

---
+++ 40_alterarAtributos/js/scripts.js
@@ -0,0 +1,12 @@
+var title = document.querySelector("#title");
+
+title.setAttribute("class", "testando atributo");
+
+console.log(title);
+
+var p = document.querySelector("#paragrafo");
+
+p.setAttribute("class", "testando novamente");
+
+console.log(p)
+;
