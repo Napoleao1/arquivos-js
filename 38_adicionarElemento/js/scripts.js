7ece8d4e4be3d87afe28c0c17f4403fe1312d92a
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-23 21:40:03 -0300

    inserting element child

--- 38_adicionarElemento/js/scripts.js
+++ 38_adicionarElemento/js/scripts.js
@@ -6,3 +6,12 @@ var elemento = document.createElement("div");
 elemento.classList = "div-criada"
 
 console.log(elemento);
+
+
+// inserindo no container elemento filho
+
+var container = document.querySelector("#container");
+
+container.appendChild(elemento);
+
+
