76954df45dcd6c69c3baadb66b5f680d9f9f1a5c
Napoleao1 <ernaninandoxd@gmail.com>
2023-06-25 18:51:23 -0300

    clearTimeout

--- 45_clearTimeout_clearInterval/js/scripts.js
+++ 45_clearTimeout_clearInterval/js/scripts.js
@@ -1,1 +1,20 @@
 // clear timeout
+
+var x = 0;
+
+var myTimer = setTimeout(function() {
+
+    console.log("o x é 0");
+
+}, 1500);
+
+x = 5;
+
+if(x > 0){
+
+    clearTimeout(myTimer);
+    console.log(" o x passou de 0 e agora é: " + x);
+
+}
+
+//clearinterval
